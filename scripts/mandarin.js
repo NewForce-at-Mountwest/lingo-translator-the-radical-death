f32762da09af06996b46ac93390eba59839a0763
Daryl Adkins <daryl.adkins@gmail.com>
2019-08-22 11:42:43 -0400

    xlate button created

--- scripts/mandarin.js
+++ scripts/mandarin.js
@@ -43,120 +43,111 @@ const mandarinData = {
 const navigation = document.querySelector("#mandarin").addEventListener("click", function () {
   const mandarinInfo = document.querySelector("#language-container")
   //inserts the lists into the correct HTML.
-  mandarinInfo.innerHTML =  `<h1>${mandarinName}</h1>
+  mandarinInfo.innerHTML = `<div class=header><h1>${mandarinName}</h1></div>
+
   <br>
   <div class="container">
-  
-  <div class="col-sm">
+    <div class="row">
+      <div class="col-sm">
   <h2>NOTABLE PEOPLE</h2>
   ${addNotablePeople()}
   </div>
   
   <br>
   <br>
-  <div class="col-sm">
+      <div class="col-sm">
   <h2>FUN FACTS</h2>
   <br>
-  <h3>Related Languages</h3>
+  <h4>Related Languages</h4>
   ${addFunFactsFactsLanguage()}
   <br>
-  <h3>Letters in the Alphabet</h3>
+  <h4>Letters in the Alphabet</h4>
   <li>${letters}</li>
-  <h3>Number of Speakers</h3>
+  <br><br>
+  <h4>Number of Speakers</h4>
   <li>${speakers}</li>
-  <br>
-  <h3>Area Dialects</h3>
+  <br><br>
+  <h4>Area Dialects</h4>
   <li>${dialectInformation}</li>
-  <li>${addDialects()}</li>
+  <br><br>
+  ${addDialects()}
   </div>
   
-  <div class="col-sm">
+      <div class="col-sm">
   <h2>COUNTRIES SPOKEN</h2>
-  ${buildcountryspokenlist()}
+  ${addCountries()}
   </div>
+  </div>
+
+  <br><br>
+
+  
+  <div class="row">
+  <div class="col">
+  <section class=textbox>
+  <input>Enter Text</input> <button class=translate_button>Translate</button>
+  </section>
   </div>
   `
-  })
-  
-  
+})
 
-
+//build translator function
+const xlate= function buildTranslatorButton(){
+  const xlateBtn = document.querySelector("body").addEventListener("click", function () {
+    //compare input text to language object
+    //get user input, make variable
+    //if statements for all words as strings
+    //print translated 
+  
+  console.log("xlate button clicked")
+})
+}
 
 
 //build list of languages
+function addFunFactsFactsLanguage(FunfactsLanguagesArray) {
 
-
-function addFunFactsFactsLanguage(FunfactsLanguagesArray){
-  
   let languageIterations = ""
- for (i = 0; i < mandarinData.funFacts.relatedLanguages.length; i++) {
-  languageIterations += `<p>${mandarinData.funFacts.relatedLanguages[i]}</p>`
- }
-   return languageIterations
+  for (i = 0; i < mandarinData.funFacts.relatedLanguages.length; i++) {
+    languageIterations += `<p><li>${mandarinData.funFacts.relatedLanguages[i]}</li></p>`
+  }
+  return languageIterations
 
 }
-//build remaining funfacts
-
-
-
-
-
 
 //build list of speakers
-function addNotablePeople(notablePeopleArray){
+function addNotablePeople(notablePeopleArray) {
   let peopleIterations = ""
- for (i = 0; i < mandarinData.notablePeople.length; i++) {
-  peopleIterations += `<p>${mandarinData.notablePeople[i]}</p>`
- }
+  for (i = 0; i < mandarinData.notablePeople.length; i++) {
+    peopleIterations += `<p><li>${mandarinData.notablePeople[i]}</li></p>`
+  }
   return peopleIterations
 
 }
 
-
-
-  
 //build list of countries in which its spoken
-function addCountries(countriesArray){
-  let countriesIterations = "<h3>Countries in which its spoken: </h3>"
- for (i = 0; i < mandarinData.countriesSpoken.length; i++) {
-  countriesIterations += `<p>${mandarinData.countriesSpoken[i]}</p>`
- }
-   return countriesIterations
+function addCountries(countriesArray) {
+  let countriesIterations = "<h3></h3>"
+  for (i = 0; i < mandarinData.countriesSpoken.length; i++) {
+    countriesIterations += `<p><li>${mandarinData.countriesSpoken[i]}</li></p>`
+  }
+  return countriesIterations
 
 }
- 
-function addDialects(dialectsArray){
-  let dialectsIterations = "<h4>Chinese Dialects: </h4>"
- for (i = 0; i < mandarinData.funFacts.mandrinInfo.chineseDialects.length; i++) {
-  dialectsIterations += `<p>${mandarinData.funFacts.mandrinInfo.chineseDialects[i]}</p>`
- }
-   return dialectsIterations
+//build list of Chinese dialect information
+function addDialects(dialectsArray) {
+  let dialectsIterations = "<h4>Chinese Dialects </h4>"
+  for (i = 0; i < mandarinData.funFacts.mandrinInfo.chineseDialects.length; i++) {
+    dialectsIterations += `<p><li>${mandarinData.funFacts.mandrinInfo.chineseDialects[i]}</li></p>`
+  }
+  return dialectsIterations
 
 }
- 
-
- 
- const letters=mandarinData.funFacts.lettersInAlphabet
- const dialectInformation=mandarinData.funFacts.mandrinInfo.dialectInfo
- const mandarinName = mandarinData.name
- 
-   
 
 
+//create variables for letters in alphabet, dialect information, and language name.
+const letters = mandarinData.funFacts.lettersInAlphabet
+const dialectInformation = mandarinData.funFacts.mandrinInfo.dialectInfo
+const mandarinName = mandarinData.name
 
 
-
- 
-//Parking Lot 
-// let factsRelatedLanguage = mandarinData.funFacts.relatedLanguages
-
-
-
-
-  
-
-
-  
-
-
- 
